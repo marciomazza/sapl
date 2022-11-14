869d7287a57228e7e8b00c5b3017f4d5bb6bfd57
Marcio Mazza <marciomazza@gmail.com>
2022-11-14 13:53:48 -0300

    Atualiza script de recriação de banco p usuario padrão postgres
    Usado por padrão no container postgres

--- sapl/legacy/scripts/recria_um_db_postgres.sh
+++ sapl/legacy/scripts/recria_um_db_postgres.sh
@@ -6,8 +6,8 @@
 set -e  # Exit immediately if a command exits with a non-zero status
 
 echo "Database $1"
-docker exec -it postgres psql -U sapl -c "drop DATABASE if exists $1"
-docker exec -it postgres psql -U sapl -c  "CREATE DATABASE $1 WITH OWNER = sapl ENCODING = 'UTF8' TABLESPACE = pg_default LC_COLLATE = 'pt_BR.UTF-8' LC_CTYPE = 'pt_BR.UTF-8' CONNECTION LIMIT = -1 TEMPLATE template0;"
+docker exec -it postgres psql -U postgres -c "drop DATABASE if exists $1"
+docker exec -it postgres psql -U postgres -c  "CREATE DATABASE $1 WITH OWNER = sapl ENCODING = 'UTF8' TABLESPACE = pg_default LC_COLLATE = 'pt_BR.UTF-8' LC_CTYPE = 'pt_BR.UTF-8' CONNECTION LIMIT = -1 TEMPLATE template0;"
 
 
 echo "--- DJANGO MIGRATE ---" | tee -a $LOG
